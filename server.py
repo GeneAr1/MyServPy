5493da67c34459aaf230097e4ca50d06079005ca
Gene Arsenault <geneaar@outlook.com>
2020-04-04 16:12:44 -0400

    Updated Contact ver 1.0a
    Updated contact form to send data using POST
    
    added thank you page

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 """ @app.route('/')
@@ -15,6 +15,16 @@ def home_page():
 def html_render_page(page_name):
     return render_template(page_name)
 
+# added submit form route 
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    #return 'form submitted!'
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/thankyou.html')
+    else:
+         return 'something went wrong, Try Again'
 
 #removed below rev 1.0 4/4/20
 """ @app.route('/about.html')
